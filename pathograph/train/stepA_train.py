98efc1a445dcf9141e3fae8c1a49aed469391792
Julian <juliansjuan08@gmail.com>
2026-02-22 13:40:32 -0800

    Add stepA_train compat shim for Vertex stepA_entry

---
+++ pathograph/train/stepA_train.py
@@ -0,0 +1,9 @@
+﻿from __future__ import annotations
+
+def main(argv=None):
+    # Delegate to the current training entrypoint
+    from pathograph.train.train_stmm import main as _main
+    return _main(argv)
+
+if __name__ == '__main__':
+    raise SystemExit(main())
